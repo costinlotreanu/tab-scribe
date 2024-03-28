2982a6969dad7ac8e8f420afc51d83b2e7985d5a
Costin Lotreanu <costin.lotreanu@gmail.com>
2024-03-28 13:55:37 +0200

    remove scripting

--- script.js
+++
@@ -1,7 +0,0 @@
-import setupTheme from './script/setup-theme.js';
-import saveOnClick from './script/save-on-click.js';
-import updateItems from './script/update-items.js';
-
-setupTheme();
-updateItems();
-saveOnClick();
